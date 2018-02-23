d695faba3814ca02d3ee224667bf7889bc139f80
ValiantsinDziatlau <valiantsindziatlau@gmail.com>
2018-02-23 21:29:16 +0300

    zeros/feature: count

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let count=0;
+    let tempNumber=5;
+    while(tempNumber<=number){
+        let CurrentNumber=tempNumber;
+        while(CurrentNumber%5===0){
+            CurrentNumber/=5;
+            count++;
+        }
+        tempNumber+=5;
+    }
+    return count;
 }
